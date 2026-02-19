f74bd2d8294e7999e5ec3755153c484583e70657
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-19 13:55:23 +0530

    initialize project structure and add configuration files

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,13 @@
  * and displays structured output via Serial Monitor.
  */
 
- // TODO 1:
- // Define LDR analog pin (Use A0)
 
- // TODO 2:
- // Create variable to store sensor reading
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
 
-    // TODO 4:
-    // Print system initialization message
 }
 
 void loop() {
 
-    // TODO 5:
-    // Read analog value from LDR
-
-    // TODO 6:
-    // Print raw ADC value
-
-    // TODO 7:
-    // Apply threshold logic (Bright / Dark detection)
-
-    // TODO 8:
-    // Print brightness status
-
-    // TODO 9:
-    // Add delay (500ms or 1 second)
 }
